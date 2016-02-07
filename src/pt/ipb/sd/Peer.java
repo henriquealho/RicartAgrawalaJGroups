ff0e2bdc4c52eef1906794bea55a5ee8224eb301
Henrique Alho <henriquev16@gmail.com>
2016-02-07 17:34:55 +0000

    Comments, CriticalSection state, Bug fixes
    It was missing the CriticalSection state control in Receive method.
    Fixed bugs on updates to Queues.
    Commented all lines.
    
    From now the solution can handle up to 4 Peer's changing messages to Enter
    Critical Section

--- src/pt/ipb/sd/Peer.java
+++ src/pt/ipb/sd/Peer.java
@@ -27,13 +27,13 @@ public class Peer extends ReceiverAdapter {
 	Peer() {
 		ReqQ = new LinkedList<PeerInfo>();
 		AckQ = new LinkedList<PeerInfo>();
-		this.state = State.ready;
+		this.state = State.ready; // Initial Peer state
 	}
 
 	public void start() throws Exception {
 		channel = new JChannel();
 		channel.setReceiver(this);
-		channel.connect("MyCluster");
+		channel.connect("MyCluster"); // Connect to Cluster "MyCluster"
 
 		try {
 			// Obtain Peer id in Cluster
@@ -49,13 +49,7 @@ public class Peer extends ReceiverAdapter {
 		Message msg = new Message(null, null, this.peerInfo);
 		try {
 			channel.send(msg);
-			ReqQ.add(this.peerInfo);
-			AckQ.add(this.peerInfo);
 			this.peerInfo.incrementLogicalClock();
-
-			this.state = State.waiting;
-			System.out.println(
-					this.peerInfo.getGuid() + ": " + this.state + ", Clock: " + this.peerInfo.getLogicalClock());
 
 		} catch (Exception e) {
 			System.out.println("ERROR: Could not send message to Cluster.\nException: " + e);
@@ -68,7 +62,7 @@ public class Peer extends ReceiverAdapter {
 
 		this.peerInfo.incrementLogicalClock();
 		System.out.println(this.peerInfo.getGuid() + ": " + this.state + ", Clock: " + this.peerInfo.getLogicalClock());
-		
+
 		// Obtain PeerInfo from message
 		PeerInfo peerInfo = (PeerInfo) msg.getObject();
 
@@ -79,51 +73,77 @@ public class Peer extends ReceiverAdapter {
 
 		// Only replies if the Peer is not in CS nor waiting for a Reply
 		if (this.state == State.ready) {
-			reply(msg.getSrc(), this.peerInfo);
+			// If message is not a Request (get.Dest() != null), it is a reply
+			// then -> Add to Acknowldege Queue and change state to waiting
+			if (msg.getDest() != null) {
+
+				// Once AckQ.contains(peerInfo) does not work as we want...
+				boolean contains = false;
+				for (PeerInfo item : AckQ) {
+					if (peerInfo.getGuid().equals(item.getGuid())) {
+						contains = true;
+					}
+				}
+				if (!contains) {
+					AckQ.add(peerInfo);
+				}
+
+				this.state = State.waiting;
+				System.out.println(
+						this.peerInfo.getGuid() + ": " + this.state + ", Clock: " + this.peerInfo.getLogicalClock());
+
+			} else {
+				// If is self Peer, change state to waiting and Reply to itself
+				if (peerInfo.getGuid().equals(this.peerInfo.getGuid())) {
+					this.state = State.waiting;
+					reply(msg.getSrc(), this.peerInfo);
+				} else {
+					// If is another Peer, reply
+					reply(msg.getSrc(), this.peerInfo);
+				}
+			}
 		}
 
 		// If Peer is waiting for replies to enter Critical Section
 		else if (this.state == State.waiting) {
+
 			// Obtain number of connected Peer's in Cluster
 			this.numberOfPeersInCluster = channel.getView().getMembers().size();
-			
-			// Message is a Request when msg.getDest() == null (multicast)
-			if(msg.getDest() == null) {
-				if(ReqQ.isEmpty()) {
-					ReqQ.add(peerInfo);
-				} else {
-					// Once ReqQ.contains(peerInfo) does not work as we want...
-					boolean contains = false;
-					for (PeerInfo item : ReqQ) {
-						if(peerInfo.getGuid().equals(item.getGuid())) {
-							contains = true;
-						}
-					}
-					if(!contains){
-						ReqQ.add(peerInfo);
-					}
-				}
-			} 
-			
-			// msg.getDest() != null -> The message is a Reply
-			else {
-				// Once AckQ.contains(peerInfo) does not work the way we want...
-				if (AckQ.isEmpty()) {
-					AckQ.add(peerInfo);
-				} else {
-					// Once AckQ.contains(peerInfo) does not work as we want...
-					boolean contains = false;
-					for (PeerInfo item : AckQ) {
-						if(peerInfo.getGuid().equals(item.getGuid())) {
-							contains = true;
-						}
-					}
-					if(!contains){
-						AckQ.add(peerInfo);
-					}
-				}
-			}			
 
+			// Message is a Request when msg.getDest() == null (multicast)
+			if (msg.getDest() == null) { // If request, add to Request Queue
+
+				// Once ReqQ.contains(peerInfo) does not work as we want...
+				boolean contains = false;
+				for (PeerInfo item : ReqQ) {
+					if (peerInfo.getGuid().equals(item.getGuid())) {
+						contains = true;
+					}
+				}
+				if (!contains) {
+					ReqQ.add(peerInfo);
+				}
+			}
+
+			// msg.getDest() != null -> The message is a Reply then, add to
+			// Acknowledge Queue
+			else {
+				// Once AckQ.contains(peerInfo) does not work as we want...
+				boolean contains = false;
+				for (PeerInfo item : AckQ) {
+					if (peerInfo.getGuid().equals(item.getGuid())) {
+						contains = true;
+					}
+				}
+				if (!contains) {
+					AckQ.add(peerInfo);
+				}
+			}
+
+			/*
+			 * Once the number of Peer's in Cluster is equals to the Acknowledge
+			 * Queue, Peer can enter Critical Section
+			 */
 			if (this.numberOfPeersInCluster == AckQ.size()) {
 
 				this.state = State.inCriticalSection;
@@ -138,20 +158,41 @@ public class Peer extends ReceiverAdapter {
 				System.out.println(
 						this.peerInfo.getGuid() + ": " + this.state + ", Clock: " + this.peerInfo.getLogicalClock());
 
-				// Reply to Requester's with lowest logicalClock
+				// Reply to Requester's, gives priority to lowest logicalClock
 				// NEEDS TO GIVE PRIORITY TO LOWEST LOGICALCLOCK <<<<<<
-				if(!this.ReqQ.isEmpty()) {
-					for (PeerInfo item : ReqQ) {
-						if (!item.getGuid().equals(this.peerInfo.getGuid())) {
+				if (!this.ReqQ.isEmpty()) {
+					for (PeerInfo item : this.ReqQ) {
+						if (item.getGuid().equals(this.peerInfo.getGuid())) {
+							// this.ReqQ.remove(item);
+						} else {
 							reply(item.getAddress(), this.peerInfo);
+							// this.ReqQ.remove(item);
 						}
-						this.ReqQ.remove(item);
 					}
 				}
+				// Clear Queues
+				this.ReqQ.clear();
+				this.AckQ.clear();
+			}
+		}
+
+		// Peer is in Critical Section
+		else { // If in Critical Section, add to Request Queue
+
+			// Once ReqQ.contains(peerInfo) does not work as we want...
+			boolean contains = false;
+			for (PeerInfo item : ReqQ) {
+				if (peerInfo.getGuid().equals(item.getGuid())) {
+					contains = true;
+				}
+			}
+			if (!contains) {
+				ReqQ.add(peerInfo);
 			}
 		}
 	}
 
+	// Message reply method, sends message to SrcAddress and PeerInfo from SrcAddress
 	public void reply(Address msgSrcAddress, PeerInfo peerInfo) {
 		Message msg = new Message(msgSrcAddress, peerInfo);
 		try {
